4dda1615c8d5d5149f9dc220fbb839b63cbde338
saifaliaxioned <saifalis@axioned.com>
2023-02-06 13:14:56 +0530

    code updated with array of object

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,124 +1,125 @@
-const form = document.querySelector('.event-form'),
-  inputGroup = document.querySelectorAll('.input-group'),
-  eventDate = document.querySelector('.event-date'),
-  startTime = document.querySelector('.start-time'),
-  endTime = document.querySelector('.end-time'),
-  eLocation = document.querySelector('.location'),
-  eName = document.querySelector('.name'),
-  stringPattern = /^[a-zA-Z0-9]{3,}\s+$/,
-  eventSection = document.querySelector('.events-section'),
-  eventItems = document.querySelector('.event-items'),
-  eventMessage = document.querySelector('.event-message'),
-  localData = JSON.parse(localStorage.getItem('itemName'));
-let  data = localData ? localData : [],arr;
+// const form = document.querySelector('.event-form'),
+//   inputGroup = document.querySelectorAll('.input-group'),
+//   eventDate = document.querySelector('.event-date'),
+//   startTime = document.querySelector('.start-time'),
+//   endTime = document.querySelector('.end-time'),
+//   eLocation = document.querySelector('.location'),
+//   eName = document.querySelector('.name'),
+//   stringPattern = /^[a-zA-Z0-9]{3,}\s+$/,
+//   eventSection = document.querySelector('.events-section'),
+//   eventItems = document.querySelector('.event-items'),
+//   eventMessage = document.querySelector('.event-message'),
+//   localData = JSON.parse(localStorage.getItem('itemName'));
+// let  data = localData ? localData : [],arr;
 
-// function to store data in local storage
-const storeData = (eventDate, startTime, endTime, eLocation, eName) => {
-  const dataObj = {
-    eventDate: eventDate,
-    startTime: startTime,
-    endTime: endTime,
-    eLocation: eLocation,
-    eName: eName
-  }
-  data.push(dataObj)
-  localStorage.setItem('itemName', JSON.stringify(data));
-}
+// // function to store data in local storage
+// const storeData = (eventDate, startTime, endTime, eLocation, eName) => {
+//   const dataObj = {
+//     eventDate: eventDate,
+//     startTime: startTime,
+//     endTime: endTime,
+//     eLocation: eLocation,
+//     eName: eName
+//   }
+//   data.push(dataObj)
+//   localStorage.setItem('itemName', JSON.stringify(data));
+// }
 
-if (form) {
-  // input validation
-  const validateInput = (input, pattern, err) => {
-    if (input.value) {
-      input.nextElementSibling.classList.remove("show-error");
-      if (pattern.test(input.value)) {
-        input.nextElementSibling.classList.remove("show-error");
-        return true;
-      } else {
-        input.nextElementSibling.classList.add("show-error");
-        input.nextElementSibling.innerText = err;
-        return false;
-      }
-    } else {
-      input.nextElementSibling.classList.add("show-error");
-      input.nextElementSibling.innerText = '*Field is required';
-      return false;
-    }
-  }
-  // submit function
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    if (eventDate.value && startTime.value && endTime.value && eLocation.value && eName.value) {
-      storeData(eventDate.value, startTime.value, endTime.value, eLocation.value, eName.value);
-      location.href = 'events.html';
-      form.reset();
-    } else {
-      validateInput(eventDate);
-      validateInput(startTime);
-      validateInput(endTime);
-      validateInput(eLocation);
-      validateInput(eName, stringPattern, '*Please enter minimum three characters');
-    }
-  });
-}
-// to sort data by date and time
-const sortData = (a, b) => { return new Date(`${(a.eventDate)} ${(a.startTime)}`) - new Date(`${(b.eventDate)} ${(b.startTime)}`); }
-// to group data by year and month wise
-const groupData = (data) => {
-  let temp = {};
-  data.forEach(obj => {
-    let showDate = obj.eventDate;
-    const yearDate = new Date(showDate).getFullYear();
-    const monthDate = new Date(showDate).toLocaleString('default', { month: 'long' });
-    temp[yearDate] ??= {};
-    temp[yearDate][monthDate] ??= [];
-    temp[yearDate][monthDate].push(obj);
-  });
-  console.log(temp);
-  showData(temp);
-}
-// to show data in UI
-const showData = (data) => {
-  for (const x in data) {
-    for (const y in data[x]) {
-      if (data[x][y] != undefined) {
-        const ul = document.createElement('ul');
-        ul.classList.add('event-data');
-        const li = document.createElement('li');
-        li.classList.add('event-list');
-        data[x][y].map((objList) => {
-          let eMonth = new Date(objList.eventDate).toLocaleString('default', { month: 'long' }),
-            eDay = new Date(objList.eventDate).toLocaleString("en", { weekday: "long" }),
-            [sH, sM] = objList.startTime.split(':');
-          [eH, eM] = objList.endTime.split(':');
-          li.innerHTML = `<h3><span class="event-month">${eMonth}</span> <span class="event-year">${new Date(objList.eventDate).getFullYear()}</span></h3>
-          `;
-          ul.innerHTML += `<li class="data-list">
-              <div class="date-data">
-                <span class="schedule-date">${new Date(objList.eventDate).getDate()}</span>
-                <span class="schedule-month">${new Date(objList.eventDate).toLocaleString('default', { month: 'short' })}</span>
-              </div>
-              <div class="event-details">
-                <h4><span class="event-day">${eDay}</span> <span class="start-event">${sH==12 ? 12 : sH >= 12 ? sH - 12 : sH}:${sM}${sH >= 12 ? 'PM' : 'AM'}</span>-<span class="end-event">${eH==12 ? 12 : eH >= 12 ? eH - 12 : eH}:${eM}${eH >= 12 ? 'PM' : 'AM'}</span></h4>
-                <p class="event-location">${objList.eLocation}</p>
-                <h5 class="event-name">${objList.eName}</h5>
-              </div>
-            </li>
-        </li>`
-        })
+// if (form) {
+//   // input validation
+//   const validateInput = (input, pattern, err) => {
+//     if (input.value) {
+//       input.nextElementSibling.classList.remove("show-error");
+//       if (pattern.test(input.value)) {
+//         input.nextElementSibling.classList.remove("show-error");
+//         return true;
+//       } else {
+//         input.nextElementSibling.classList.add("show-error");
+//         input.nextElementSibling.innerText = err;
+//         return false;
+//       }
+//     } else {
+//       input.nextElementSibling.classList.add("show-error");
+//       input.nextElementSibling.innerText = '*Field is required';
+//       return false;
+//     }
+//   }
+//   // submit function
+//   form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     if (eventDate.value && startTime.value && endTime.value && eLocation.value && eName.value) {
+//       storeData(eventDate.value, startTime.value, endTime.value, eLocation.value, eName.value);
+//       location.href = 'events.html';
+//       form.reset();
+//     } else {
+//       validateInput(eventDate);
+//       validateInput(startTime);
+//       validateInput(endTime);
+//       validateInput(eLocation);
+//       validateInput(eName, stringPattern, '*Please enter minimum three characters');
+//     }
+//   });
+// }
+// // to sort data by date and time
+// const sortData = (a, b) => { return new Date(`${(a.eventDate)} ${(a.startTime)}`) - new Date(`${(b.eventDate)} ${(b.startTime)}`); }
+// // to group data by year and month wise
+// const groupData = (data) => {
+//   let temp = {};
+//   data.forEach(obj => {
+//     let showDate = obj.eventDate;
+//     const yearDate = new Date(showDate).getFullYear();
+//     const monthDate = new Date(showDate).toLocaleString('default', { month: 'long' });
+//     temp[`${monthDate}${yearDate}`] ??= [];
+//     // temp[yearDate][monthDate] ??= [];
+//     temp[`${monthDate}${yearDate}`].push(obj);
+//   });
+//   console.log(temp);
+//   showData(temp);
+// }
+// // to show data in UI
+// const showData = (data) => {
+//   for (const x in data) {
+//     // for (const y in data[x]) {
+//       if (data[x] != undefined) {
+//         const ul = document.createElement('ul');
+//         ul.classList.add('event-data');
+//         const li = document.createElement('li');
+//         li.classList.add('event-list');
+//         data[x].map((objList) => {
+//           let eMonth = new Date(objList.eventDate).toLocaleString('default', { month: 'long' }),
+//             eDay = new Date(objList.eventDate).toLocaleString("en", { weekday: "long" }),
+//             [sH, sM] = objList.startTime.split(':');
+//           [eH, eM] = objList.endTime.split(':');
+//           li.innerHTML = `<h3><span class="event-month">${eMonth}</span> <span class="event-year">${new Date(objList.eventDate).getFullYear()}</span></h3>
+//           `;
+//           ul.innerHTML += `<li class="data-list">
+//               <div class="date-data">
+//                 <span class="schedule-date">${new Date(objList.eventDate).getDate()}</span>
+//                 <span class="schedule-month">${new Date(objList.eventDate).toLocaleString('default', { month: 'short' })}</span>
+//               </div>
+//               <div class="event-details">
+//                 <h4><span class="event-day">${eDay}</span> <span class="start-event">${sH==12 ? 12 : sH >= 12 ? sH - 12 : sH}:${sM}${sH >= 12 ? 'PM' : 'AM'}</span>-<span class="end-event">${eH==12 ? 12 : eH >= 12 ? eH - 12 : eH}:${eM}${eH >= 12 ? 'PM' : 'AM'}</span></h4>
+//                 <p class="event-location">${objList.eLocation}</p>
+//                 <h5 class="event-name">${objList.eName}</h5>
+//               </div>
+//             </li>
+//         </li>`
+//         })
 
-        li.appendChild(ul);
-        eventItems.appendChild(li);
-      }
-    }
-  }
-}
-if (eventSection) {
-  const onload = (data) => {
-    arr = data.sort(sortData);
-    groupData(arr);
-  }
-  onload(data);
-}
+//         li.appendChild(ul);
+//         eventItems.appendChild(li);
+//       }
+//     // }
+//   }
+// }
+// if (eventSection) {
+//   const onload = (data) => {
+//     arr = data.sort(sortData);
+//     console.log(arr);
+//     groupData(arr);
+//   }
+//   onload(data);
+// }
 
 
 
---
+++ assets/js/script3.js
@@ -0,0 +1,142 @@
+const form = document.querySelector('.event-form'),
+  inputGroup = document.querySelectorAll('.input-group'),
+  eventDate = document.querySelector('.event-date'),
+  startTime = document.querySelector('.start-time'),
+  endTime = document.querySelector('.end-time'),
+  eLocation = document.querySelector('.location'),
+  eName = document.querySelector('.name'),
+  stringPattern = /^[a-zA-Z0-9]{3,}\s+$/,
+  eventSection = document.querySelector('.events-section'),
+  eventItems = document.querySelector('.event-items'),
+  eventMessage = document.querySelector('.event-message'),
+  localData = JSON.parse(localStorage.getItem('itemName'));
+let data = localData ? localData : [], arr;
+
+// function to store data in local storage
+const storeData = (eventDate, startTime, endTime, eLocation, eName) => {
+  const dataObj = {
+    eventDate: eventDate,
+    startTime: startTime,
+    endTime: endTime,
+    eLocation: eLocation,
+    eName: eName
+  }
+  data.push(dataObj)
+  localStorage.setItem('itemName', JSON.stringify(data));
+}
+
+if (form) {
+  // input validation
+  const validateInput = (input, pattern, err) => {
+    if (input.value) {
+      input.nextElementSibling.classList.remove("show-error");
+      if (pattern.test(input.value)) {
+        input.nextElementSibling.classList.remove("show-error");
+        return true;
+      } else {
+        input.nextElementSibling.classList.add("show-error");
+        input.nextElementSibling.innerText = err;
+        return false;
+      }
+    } else {
+      input.nextElementSibling.classList.add("show-error");
+      input.nextElementSibling.innerText = '*Field is required';
+      return false;
+    }
+  }
+  // submit function
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (eventDate.value && startTime.value && endTime.value && eLocation.value && eName.value) {
+      storeData(eventDate.value, startTime.value, endTime.value, eLocation.value, eName.value);
+      location.href = 'events.html';
+      form.reset();
+    } else {
+      validateInput(eventDate);
+      validateInput(startTime);
+      validateInput(endTime);
+      validateInput(eLocation);
+      validateInput(eName, stringPattern, '*Please enter minimum three characters');
+    }
+  });
+}
+// to sort data by date and time
+const sortData = (a, b) => { return new Date(`${(a.eventDate)} ${(a.startTime)}`) - new Date(`${(b.eventDate)} ${(b.startTime)}`); }
+
+const uiData = (data) => {
+  let year = month = null, ul, li;
+  data.map((objList) => {
+    [yearTemp, monthTemp] = objList.eventDate.split('-');
+    let eMonth = new Date(objList.eventDate).toLocaleString('default', { month: 'long' }),
+      eYear = new Date(objList.eventDate).getFullYear();
+
+    if (year == yearTemp && month == monthTemp) {
+      addLi(objList, ul);
+
+    } else {
+      year = yearTemp;
+      month = monthTemp;
+      ul = document.createElement('ul');
+      ul.classList.add('event-data');
+      li = document.createElement('li');
+      li.classList.add('event-list');
+      
+      li.innerHTML = `<h3><span class="event-month">${eMonth}</span> <span class="event-year">${eYear}</span></h3>`;
+      addLi(objList, ul);
+      if (li !== undefined) {
+        li.appendChild(ul);
+        eventItems.appendChild(li);
+      }
+
+    }
+  })
+}
+
+const addLi = (objList, ul) => {
+  let eDay = new Date(objList.eventDate).toLocaleString("en", { weekday: "long" }),
+    [sH, sM] = objList.startTime.split(':'),
+    [eH, eM] = objList.endTime.split(':');
+
+  ul.innerHTML += `<li class="data-list">
+              <div class="date-data">
+                <span class="schedule-date">${new Date(objList.eventDate).getDate()}</span>
+                <span class="schedule-month">${new Date(objList.eventDate).toLocaleString('default', { month: 'short' })}</span>
+              </div>
+              <div class="event-details">
+                <h4><span class="event-day">${eDay}</span> <span class="start-event">${sH == 12 ? 12 : sH >= 12 ? sH - 12 : sH}:${sM}${sH >= 12 ? 'PM' : 'AM'}</span>-<span class="end-event">${eH == 12 ? 12 : eH >= 12 ? eH - 12 : eH}:${eM}${eH >= 12 ? 'PM' : 'AM'}</span></h4>
+                <p class="event-location">${objList.eLocation}</p>
+                <h5 class="event-name">${objList.eName}</h5>
+              </div>
+            </li>
+        </li>`;
+}
+
+if (eventSection) {
+  const onload = (data) => {
+    arr = data.sort(sortData);
+    uiData(arr);
+  }
+  onload(data);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
